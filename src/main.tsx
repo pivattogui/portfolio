1ca3cf84d1ebb4f2016443ac81c4cf10bde229dd
Pivatto <github@pivatto.dev>
2026-09-17 20:01:20 -0300

    style: use DM Sans for primary typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/barlow-condensed/400.css";
-import "@fontsource/barlow-condensed/500.css";
-import "@fontsource/barlow-condensed/600.css";
 import "@fontsource/instrument-serif/400.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
 import "@fontsource/dm-sans/600.css";
 import "lenis/dist/lenis.css";
 import "./index.css";
